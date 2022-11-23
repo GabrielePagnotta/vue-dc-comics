52afebd85ad15d90fa4faeaa6ce52c109d4fa4fb
GabrielePagnotta <gabriele.pagnotta1998@gmail.com>
2022-11-23 18:25:36 +0100

    header

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,38 @@
 <template>
   <div id="app">
+    <HeaderOne />
+    
     
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderOne from './components/header/HeaderOne.vue'
+
+
+
 
 export default {
   name: 'App',
   components: {
+   HeaderOne,
     
   }
 }
 </script>
 
 <style lang="scss">
+*{
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
