45fe72681e263f5a7b34ce4a7bccf60b6830e49c
GabrielePagnotta <gabriele.pagnotta1998@gmail.com>
2022-11-23 18:25:43 +0100

    header

---
+++ src/components/header/HeaderOne.vue
@@ -0,0 +1,81 @@
+<template>
+    <div>
+        <div class="box flex">
+
+            <section>
+                <img  src="../../assets/img/dc-logo.png" alt="logo dc comics" class="logo">
+            </section>
+            <section>
+
+                <ul class="flex">
+                    <li class="distance">CHARACHTER</li>
+                    <div class="customization-blue"><li class="distance lightblue-color">COMICS</li></div>
+                    <li class="distance">MOVIES</li>
+                    <li class="distance">TV</li>
+                    <li class="distance">GAMES</li>
+                    <li class="distance">COLLCTABLES</li>
+                    <li class="distance">VIDEOS</li>
+                    <li class="distance">FANS</li>
+                    <li class="distance">NEWS</li>
+                    <li class="distance">SHOP</li>
+                </ul>
+
+            </section>
+        </div>
+        
+        <HeaderTwo />
+    </div>
+    
+</template>
+
+<script>
+    import HeaderTwo from './HeaderTwo.vue'
+
+    export default {
+
+       name:"HaderOne",
+       components:{
+        HeaderTwo
+       }
+       
+    }
+</script>
+
+<style lang="scss" scoped>
+.box{
+    width:100%;
+    justify-content: space-around;
+    align-items: center;
+}
+.flex{
+    display: flex;
+}
+
+.logo{
+    width: 90%;
+    padding: 10px;
+}
+
+.distance{
+    margin: 0 20px;
+    font-weight: bold;
+    color: black;
+}
+
+.customization-blue{
+    display: flex;
+    align-items: center;
+    height: 115px;
+    border-bottom: 5px solid #0C7CEC;
+    padding-top: 5px;
+}
+
+.lightblue-color{
+    color: #0C7CEC;
+}
+
+li{
+    display: flex;
+    align-items: center;
+}
+</style>
---
+++ src/components/header/HeaderTwo.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div class="box flex">
+            <h1> -->Content goes here -- </h1>
+        </div>
+        <div></div>
+    </div>
+</template>
+
+<script>
+    export default {
+       name: "HeaderTwo"
+    }
+</script>
+
+<style lang="scss" scoped>
+.box{
+    width: 100%;
+    padding: 50px;
+    background-color: black;
+    justify-content: flex-start;
+    color: white;
+}
+
+.flex{
+    display: flex;
+
+}
+
+h1{
+    margin:0 100px;
+}
+</style>
